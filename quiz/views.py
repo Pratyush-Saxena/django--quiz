807e4512aea155d0903b4d543c56188e2b895e11
Pratyush <pratyush@This-Acer.localdomain>
2021-05-05 16:56:12 +0530

    all updated

--- quiz/views.py
+++ quiz/views.py
@@ -10,6 +10,13 @@ easy_quest=json.loads(serializers.serialize('json',question.objects.filter(level
 hard_quest=json.loads(serializers.serialize('json',question.objects.filter(level="Hard")))
 medium_quest=json.loads(serializers.serialize('json',question.objects.filter(level="Medium")))
 
+def update_score(level):
+    if level=="Easy":
+        return 5
+    elif level=="Medium":
+        return 10
+    else:
+        return 15
 
 def index(request):
     if request.method == "POST":
@@ -21,24 +28,41 @@ def quiz(request):
         request.session['count']=0
         request.session['score']=0
         request.session['level']="Easy"
+        request.session['ques_list']=list()
         ques=easy_quest[random.randrange(0,len(easy_quest))]
     else:
         choice=eval(request.POST.get('choice'))
+        request.session['curr_ques']['usr_ans']=choice
+        request.session['ques_list'].append(request.session['curr_ques'])
+        if request.session['count']==10:
+            usr=dict()
+            usr['ques_list']=request.session['ques_list']
+            usr['score']=request.session['score']
+            return render(request,'quiz/p3.html',{'usr_data':usr})
         if choice['Answer']:
+            request.session['score']+=update_score(request.session['level'])
             if request.session['level']=="Easy":
                 ques=medium_quest[random.randrange(0,len(medium_quest))]
+                while(ques in request.session['ques_list']):
+                    ques=medium_quest[random.randrange(0,len(medium_quest))]
                 request.session['level']="Medium"
             else:
                 ques=hard_quest[random.randrange(0,len(hard_quest))]
+                while(ques in request.session['ques_list']):
+                    ques=hard_quest[random.randrange(0,len(hard_quest))]
                 request.session['level']="Hard"
         else:
             if request.session['level']=="Hard":
                 ques=medium_quest[random.randrange(0,len(medium_quest))]
+                while(ques in request.session['ques_list']):
+                    ques=medium_quest[random.randrange(0,len(medium_quest))]
                 request.session['level']="Medium"
             else:
                 ques=easy_quest[random.randrange(0,len(easy_quest))]
+                while(ques in request.session['ques_list']):
+                    ques=medium_quest[random.randrange(0,len(medium_quest))]
                 request.session['level']="Easy"
-    print(request.session['level'])
+    request.session['curr_ques']=ques
     request.session['count']+=1
     return render(request,'quiz/p2.html',{"quest":ques,"count":request.session['count']})
 
